d17833081215d7aafea4e3e1698e85c9606ae525
jme14 <williamsltrey3@gmail.com>
2025-02-03 18:12:20 -0600

    finish writing class as list and filtering and sorting by rewatch and rating respectively

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
 import fs from "fs";
 import { parse } from "csv-parse";
+import { stringify } from "csv-stringify/sync";
 import { DiaryEntry } from "../types/letterboxd";
 
 class LetterboxdData {
     diary: DiaryEntry[] = [];
     private constructor() {}
     static async init(diaryEntryArray?: DiaryEntry[]): Promise<LetterboxdData> {
-
-        if (diaryEntryArray){
-            const instance = new LetterboxdData()
-            instance.diary = diaryEntryArray
+        if (diaryEntryArray) {
+            const instance = new LetterboxdData();
+            instance.diary = diaryEntryArray;
             return instance;
         }
 
         const instance: LetterboxdData = new LetterboxdData();
         instance.diary = await instance.readDiary();
         return instance;
-
     }
     private async readDiary(): Promise<DiaryEntry[]> {
         const filePath = "data/diary.csv";
@@ -40,33 +39,79 @@ class LetterboxdData {
                 rating: Number(row["Rating"]),
                 rewatch: Boolean(row["Rewatch"]),
                 tags: row["Tags"],
-                watchedDate: row["Watched Date"]
-            }
+                watchedDate: row["Watched Date"],
+            };
             diaryEntries.push(entry);
         }
         return diaryEntries;
     }
 
-    async filterByWatchedDate(yearFilter: number, monthFilter?: number, dayFilter?: number): Promise<LetterboxdData>{
-        const diary: DiaryEntry[] = this.diary
-        const filteredDiary = diary.filter((entry) =>{
-            const watchedDate: Date = new Date(`${entry.watchedDate}T00:00:00Z`)
-            const watchedYear = watchedDate.getUTCFullYear()
-            return watchedYear == yearFilter
-        })
-        const filteredLetterboxdData = await LetterboxdData.init(filteredDiary)
+    async filterByWatchedDate(
+        yearFilter: number,
+        monthFilter?: number,
+        dayFilter?: number
+    ): Promise<LetterboxdData> {
+        const diary: DiaryEntry[] = this.diary;
+        const filteredDiary = diary.filter((entry) => {
+            const watchedDate: Date = new Date(
+                `${entry.watchedDate}T00:00:00Z`
+            );
+            const watchedYear = watchedDate.getUTCFullYear();
+            return watchedYear == yearFilter;
+        });
+        const filteredLetterboxdData = await LetterboxdData.init(filteredDiary);
         return filteredLetterboxdData;
+    }
+    async filterByRewatch(rewatch: boolean): Promise<LetterboxdData> {
+        const diary: DiaryEntry[] = this.diary;
+        const filteredDiary = diary.filter((entry) => {
+            return entry.rewatch == rewatch;
+        });
+        const filteredLetterboxdData = await LetterboxdData.init(filteredDiary);
+        return filteredLetterboxdData;
+    }
+
+    async writeDiaryAsLetterboxdList(listName: string) {
+        const listContent: string = stringify(this.diary, {
+            header: true,
+            columns: [
+                { key: "uri", header: "Letterboxd URI" },
+                { key: "name", header: "Title" },
+            ],
+        });
+
+        fs.writeFileSync(`out/${listName}.csv`, listContent);
+    }
+
+    sortDiaryByRating(descending: boolean) {
+        var returner = 1;
+        if (descending) {
+            returner = -1;
+        }
+        this.diary = this.diary.sort((a, b) => {
+            if (a.rating == undefined) {
+                return returner;
+            } else if (b.rating == undefined) {
+                return -returner;
+            }
+
+            if (a.rating >= b.rating) {
+                return returner;
+            } else {
+                return -returner;
+            }
+        });
     }
 }
 
-async function getLetterboxdData(){
+async function getLetterboxdData() {
     const lbData = await LetterboxdData.init();
     // console.log(lbData.diary)
-    const filteredLBData = await lbData.filterByWatchedDate(2024)
-    console.log(filteredLBData.diary)
-    console.log(filteredLBData.diary.length)
+    const filteredByDate = await lbData.filterByWatchedDate(2024);
+    const filteredLBData = await filteredByDate.filterByRewatch(false);
+    filteredLBData.sortDiaryByRating(true);
 
-
+    filteredLBData.writeDiaryAsLetterboxdList("2024 First Watches Ranked");
 }
 
-getLetterboxdData()
+getLetterboxdData();
